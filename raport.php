fbf17c51f7db9a463a09c4ad30535bbed0c3e3ce
Dodo <dawidmalich@gmail.com>
2024-04-22 19:04:45 +0200

    a

--- raport.php
+++ raport.php
@@ -6,19 +6,21 @@
 </head>
 <body>
 <fieldset>
-        <legend>wybierz opcję</legend>
-        <form action='wplaty.php' method='get'>
-            <input type='submit' value='wpłać'>
+    <legend>wybierz opcję</legend>
+    <form action='wplaty.php' method='get'>
+        <input type='number' min='0.01' name='ile' step='0.01' required>
+        <input type='submit' value='wpłać'>
         </form>
         <form action='wyplaty.php' method='get'>
-            <input type='submit' value='wypłać'>
-        </form>
-        <form action='inwentaryzacja.php' method='get'>
-            <input type='submit' value='inwentaryzacja'>
-        </form>
-        <form action='raport.php' method='get'>
-            <input type='submit' value='raport'>
-        </form>
+        <input type='number' min='0.01' name='kiedy'  step='0.01' required>
+        <input type='submit' value='wypłać'>
+    </form>
+    <form action='inwentaryzacja.php' method='get'>
+        <input type='submit' value='inwentaryzacja'>
+    </form>
+    <form action='raport.php' method='get'>
+        <input type='submit' value='raport'>
+    </form>
         
     </fieldset> <br>
 
