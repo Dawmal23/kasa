ce6b751ac3a3456186f65d635a09f5c1b49b96ab
Dodo <dawidmalich@gmail.com>
2024-04-23 21:15:48 +0200

    inwentaryzacja nie działa częściowo, wyświetlanie raportów, wpłaty i wypłaty

--- inwentaryzacja.php
+++ inwentaryzacja.php
@@ -1,31 +1,30 @@
-<?php echo "<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang='pl'>
 <head>
     <meta charset='UTF-8'>
+    <link rel="stylesheet" href="style.css">
     <title>Kasa</title>
 </head>
 <body>
 <fieldset>
 <legend>wybierz opcję</legend>
-    <form action='wplaty.php' method='get'>
-        <input type='number' min='0.01' name='ile' step='0.01' required>
-        <input type='submit' value='wpłać'>
-    </form>
-    <form action='wyplaty.php' method='get'>
-    <input type='number' min='0.01' name='kiedy'  step='0.01' required>
-        <input type='submit' value='wypłać'>
-    </form>
+    
     <form action='inwentaryzacja2.php' method='get'>
-        pięćsetek: <input type='number' value='0' min=0 step='0.01' name='piecset' required><br>
-        dwusetek: <input type='number' value='0' min=0 step='0.01' name='dwiescie' required><br>
-        setek: <input type='number' value='0' min=0 step='0.01' name='sto' required><br>
-        piędziesiątek: <input type='number' value='0' min=0 step='0.01' name='piecdziesiat' required><br>
-        dwudziestek: <input type='number' value='0' min=0 step='0.01' name='dwadziescia' required><br>
-        dziesiątek: <input type='number' value='0' min=0 step='0.01' name='dziesiec' required><br>
-        piątek: <input type='number' value='0' min=0 step='0.01' name='piatec' required><br>
-        dwójek: <input type='number' value='0' min=0 step='0.01' name='dwojek' required><br>
-        jedynek: <input type='number' value='0' min=0 step='0.01' name='jedynek' required><br>
-        
+        pięćsetek: <input type='number' value='0' min=0 step='1' name='piecset' required><br>
+        dwusetek: <input type='number' value='0' min=0 step='1' name='dwiescie' required><br>
+        setek: <input type='number' value='0' min=0 step='1' name='sto' required><br>
+        piędziesiątek: <input type='number' value='0' min=0 step='1' name='piecdziesiat' required><br>
+        dwudziestek: <input type='number' value='0' min=0 step='1' name='dwadziescia' required><br>
+        dziesiątek: <input type='number' value='0' min=0 step='1' name='dziesiec' required><br>
+        piątek: <input type='number' value='0' min=0 step='1' name='piatec' required><br>
+        dwójek: <input type='number' value='0' min=0 step='1' name='dwojek' required><br>
+        jedynek: <input type='number' value='0' min=0 step='1' name='jedynek' required><br>
+        piędziesięcio groszówek: <input type='number' value='0' min=0 step='1' name='pisiondgr' required><br>
+        dwudziesto groszówek: <input type='number' value='0' min=0 step='1' name='dwadziesciagr' required><br>
+        dziesięcio groszówek: <input type='number' value='0' min=0 step='1' name='dziesiecgr' required><br>
+        pięcio groszówek: <input type='number' value='0' min=0 step='1' name='piecgr' required><br>
+        dwu groszówek: <input type='number' value='0' min=0 step='1' name='dwagr' required><br>
+        groszówek: <input type='number' value='0' min=0 step='1' name='gr' required><br>
         <input type='submit' value='podlicz'>
         
     </form>
@@ -34,14 +33,5 @@
     </fieldset> <br>
 
 
-";
-$banknoty=[];
-
-
-
-$banknoty[0] = $_GET['piecset'];
-
-echo '</body>
-</html>';
-
-?>
+</body>
+</html>
--- inwentaryzacja2.php
+++ inwentaryzacja2.php
@@ -2,10 +2,13 @@
 <html lang='pl'>
 <head>
     <meta charset='UTF-8'>
+    <link rel='stylesheet' href='style.css'>
     <title>Kasa</title>
 </head>
 <body>
+<div id='szynka'>
 <fieldset>
+
         <legend>wybierz opcję</legend>
         <form action='wplaty.php' method='get'>
             <input type='number' min='0.01' name='ile' step='0.01' required>
@@ -23,18 +26,87 @@
         </form>
         
     </fieldset> <br>
-
+</div>
 
 ";
+$najpierw=0;
+$banknoty =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,];
+$nominaly=[500,200,100,50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01];
+$bylo=fopen("plik.txt", 'r');
+$bylozaw=fread($bylo, filesize("plik.txt"));
+    $k=(explode(",",$bylozaw));
+    for($i=0;$i<=14;$i++){
+        $najpierw=$najpierw+$k[$i]*$nominaly[$i];
 
+    }
 
+    $tab=[];
+    $tab[0] = $_GET['piecset'];
+    $tab[1] = $_GET['dwiescie'];
+    $tab[2] = $_GET['sto'];
+    $tab[3] = $_GET['piecdziesiat'];
+    $tab[4] = $_GET['dwadziescia'];
+    $tab[5] = $_GET['dziesiec'];
+    $tab[6] = $_GET['piatec'];
+    $tab[7] = $_GET['dwojek'];
+    $tab[8] = $_GET['jedynek'];
+    $tab[9] = $_GET['pisiondgr'];
+    $tab[10] = $_GET['dwadziesciagr'];
+    $tab[11] = $_GET['dziesiecgr'];
+    $tab[12] = $_GET['piecgr'];
+    $tab[13] = $_GET['dwagr'];
+    $tab[14] = $_GET['gr'];
+    $suma=0;
+    echo "<div id='tabela1'>
+    <table><tr><td>-</td><td>stan rzeczywisty</td><td>stan logiczny</td></tr>";
+    for ($i=0;$i<=14;$i++){
+        echo "<tr><td>";
+        if($k[$i]==$tab[$i]){
+            echo $nominaly[$i]."</td><td>".$tab[$i]."</td><td>".$k[$i]."</td></tr>";
+        }else{
+            echo $nominaly[$i]."</td><td bgcolor=#dd7373>".$tab[$i]."</td><td>".$k[$i]."</td></tr>";
+            $k[$i]=$tab[$i];
 
-
-
-
-
+        }
+        
+        $suma=$suma+$tab[$i]*$nominaly[$i];
+    } 
+    if($suma==$najpierw){ 
+    echo "<tr><td>-</td><td>$suma</td><td>$najpierw</td></table>";
+    }else{
+        echo "<tr><td>-</td><td bgcolor=#dd7373>$suma</td><td>$najpierw</td></table>";
+        if($suma>$najpierw){
+        echo "<input type='submit' id='pstryczek' value='dołuż do kasy brakujące pieniądze'";
+        }else{
+            echo "<input type='submit' id='pstryczek' value='wyjmij z kasy nadmiar pieniądzy'";
+        }
+        echo "</div>";
+    }
+    echo "<div id='tabela2'><table><tr><td>-</td><td>stan rzeczywisty</td><td>stan logiczny</td></tr>";
+    for ($i=0;$i<=14;$i++){
+        echo "<tr><td>";
+        
+            echo $nominaly[$i]."</td><td>".$tab[$i]."</td><td>".$k[$i]."</td></tr>";
+    }
+    echo "<tr><td>-</td><td>$suma</td><td>$najpierw</td></table>";
+    echo "</div>";
 
 echo '</body>
 </html>';
 
 ?>
+<script type="text/JavaScript">
+    var tabela1 = document.getElementById("tabela1")
+    var tabela2 = document.getElementById("tabela2")
+    tabela2.style.display="none"
+    var szynka = document.getElementById("szynka")
+    var pstryczek = document.getElementById("pstryczek")
+    szynka.style.display = "none"
+    pstryczek.onclick = function(){
+        szynka.style.display = ""
+        tabela2.style.display=""
+        pstryczek.style.display="none"
+        tabela1.style.display="none"
+        
+    }
+</script>
